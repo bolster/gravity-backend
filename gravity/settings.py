48d7ad6c4293071a97317b9d2162c169a76aeb4b
Josh Ourisman <josh@joshourisman.com>
2014-04-12 13:40:26 -0400

    Using dj-database-url to configure settings.DATABASES['default'].

--- gravity/settings.py
+++ gravity/settings.py
@@ -56,10 +56,7 @@ WSGI_APPLICATION = 'gravity.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': DATABASES = {'default': dj_database_url.config()}
 }
 
 # Internationalization
