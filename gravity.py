c1247b3fefc08739283e5c5acecf362fc86dcfda
Josh Ourisman <josh@joshourisman.com>
2014-04-12 14:26:03 -0400

    Added simple Flask app that hooks up a Restless API to Wolfram Alpha for querying gravitational force data.

---
+++ gravity.py
@@ -0,0 +1,39 @@
+import os
+import wolframalpha
+
+from flask import Flask
+from restless.exceptions import NotFound
+from restless.fl import FlaskResource
+
+app = Flask(__name__)
+
+APP_ID = os.environ['APP_ID']
+
+
+class LocationResource(FlaskResource):
+
+    def list(self):
+        query = dict(self.request.args).get('q', None)
+
+        if query is None:
+            raise NotFound('Location not found or not specified.')
+
+        location = query[0]
+        client = wolframalpha.Client(APP_ID)
+        result = client.query('gravitation acceleration {}'.format(location))
+        pod = [
+            pod.text for pod in result.pods
+            if pod.text and pod.text.startswith('total field')
+        ][0].split('\n')[0]
+        acceleration = float(pod.split('|')[1].strip().split(' ')[0])
+
+        return [{
+            'location': location,
+            'acceleration': acceleration,
+        }]
+
+
+LocationResource.add_url_rules(app, '/api/v1/location/', 'location')
+
+if __name__ == "__main__":
+    app.run(debug=True)
