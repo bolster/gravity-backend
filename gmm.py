3e01164f823e415bba8dd8adc3a70c419359ce06
Josh Ourisman <josh@joshourisman.com>
2014-04-13 11:40:55 -0400

    PEP8

--- gmm.py
+++ gmm.py
@@ -1,11 +1,13 @@
-from decimal import Decimal, Context, getcontext, ROUND_DOWN
-import math
+from decimal import Decimal, getcontext, ROUND_DOWN
 import requests
 import struct
 
+
 class GMMDataSource(object):
     URI_PREFIX = "http://gravityapp.s3.amazonaws.com/ga/"
-    DATA_FORMAT = ">i" # BIG endian int32 as specified in http://ddfe.curtin.edu.au/gravitymodels/GGMplus/GGMplus_readme.dat
+    # BIG endian int32 as specified in
+    # http://ddfe.curtin.edu.au/gravitymodels/GGMplus/GGMplus_readme.dat
+    DATA_FORMAT = ">i"
 
     ROW_LENGTH = 1000
     COLUMN_HEIGHT = 1000
@@ -24,12 +26,14 @@ class GMMDataSource(object):
         lat_ref = (int(lat) / 5) * 5
         lon_ref = (int(lon) / 5) * 5
         return "%s%02d%s%03d.ga" % (latitude_direction, lat_ref,
-            longitude_direction, lon_ref)
+                                    longitude_direction, lon_ref)
 
     def get_data_offset(self, lat, lon):
         c = getcontext()
-        lat_effective = c.divmod(c.abs(lat), Decimal('5.0'))[1].quantize(Decimal('.001'), rounding=ROUND_DOWN)
-        lon_effective = c.divmod(c.abs(lon), Decimal('5.0'))[1].quantize(Decimal('.001'), rounding=ROUND_DOWN)
+        lat_effective = c.divmod(c.abs(lat), Decimal('5.0'))[1].quantize(
+            Decimal('.001'), rounding=ROUND_DOWN)
+        lon_effective = c.divmod(c.abs(lon), Decimal('5.0'))[1].quantize(
+            Decimal('.001'), rounding=ROUND_DOWN)
         lon_offset = int(lon_effective / Decimal('5.0') * 1000)
         lat_offset = int(lat_effective / Decimal('5.0') * 1000)
         record_offset = lat_offset * (self.ROW_LENGTH) * 4 + lon_offset * 4
@@ -39,5 +43,6 @@ class GMMDataSource(object):
         filename = self.pick_file(lat, lon)
         url = "%s%s" % (self.URI_PREFIX, filename)
         offset = self.get_data_offset(lat, lon)
-        r = requests.get(url, headers={'Range': 'bytes=%d-%d' % (offset, offset+3)})
+        r = requests.get(
+            url, headers={'Range': 'bytes=%d-%d' % (offset, offset + 3)})
         return struct.unpack(self.DATA_FORMAT, r.content)[0] * 0.1
