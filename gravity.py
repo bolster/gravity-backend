d0ffe560d22c7d37fec6a139ad4f5b5f8fac4be6
Josh Ourisman <josh@joshourisman.com>
2014-04-15 10:56:26 -0400

    Moved caching functionality from gravity.LocationResource.list() to wolfram.WolframAlphaDataSource.get_gravitational_acceleration_at_point().

--- gravity.py
+++ gravity.py
@@ -3,7 +3,6 @@ import os
 from decimal import Decimal
 
 from flask import Flask
-from pymongo import MongoClient, GEOSPHERE
 from restless.fl import FlaskResource
 from wolfram import WolframAlphaDataSource
 
@@ -28,40 +27,9 @@ class LocationResource(FlaskResource):
             'coordinates': [Decimal(query['long']), Decimal(query['lat'])],
         }
 
-        client = MongoClient(
-            os.getenv('MONGOHQ_URL', 'mongodb://localhost:27017/gravity')
-        )
-        db = getattr(client, os.getenv('DATABASE_NAME', 'gravity'))
-        db.locations.ensure_index([('location', GEOSPHERE)])
-
-        location['coordinates'] = map(float, location['coordinates'])
-
-        try:
-            cached = db.locations.find({
-                "location": {
-                    "$nearSphere": location,
-                    '$maxDistance': 500,
-                }}).limit(1)
-            if cached.count() > 0:
-                return cached[0]
-        except:
-            pass
-
         wolfram_source = WolframAlphaDataSource(APP_ID)
-        acceleration, source = \
-            wolfram_source.get_gravitational_acceleration_at_point(
-                location['coordinates'][1], location['coordinates'][0])
-
-        result = {
-            'acceleration': acceleration,
-            'location': location,
-            'source': source,
-        }
-
-        try:
-            db.locations.insert(result)
-        except:
-            pass
+        result = wolfram_source.get_gravitational_acceleration_at_point(
+            location)
 
         return result
 
--- wolfram.py
+++ wolfram.py
@@ -1,7 +1,10 @@
+import os
 import wolframalpha
 
 from datetime import datetime
 from urllib import urlencode
+
+from pymongo import MongoClient, GEOSPHERE
 
 
 class WolframAlphaDataSource(object):
@@ -10,21 +13,13 @@ class WolframAlphaDataSource(object):
         self.app_id = app_id
         self.query = ''
 
-    def get_gravitational_acceleration_at_point(self, lat, lon):
-        client = wolframalpha.Client(self.app_id)
-        self.query = 'gravitational acceleration {} {}'.format(lat, lon)
-        result = client.query(self.query)
+    def get_gravitational_acceleration_at_point(self, location):
+        result = self.get_cached_acceleration(location) \
+            or self.query_wolfram(location)
 
-        pod = [
-            pod.text for pod in result.pods
-            if pod.text and pod.text.startswith('total field')
-        ][0].split('\n')[0]
-        acceleration = float(pod.split('|')[1].strip().split(' ')[0])
+        return result
 
-        return acceleration, self.source
-
-    @property
-    def source(self):
+    def get_source(self):
         return {
             'Author/Site': 'Wolfram|Alpha',
             'Publisher': 'Wolfram Alpha LLC',
@@ -32,3 +27,51 @@ class WolframAlphaDataSource(object):
                 urlencode({'input': self.query})),
             'Date': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S'),
         }
+
+    def get_cached_acceleration(self, location):
+        client = MongoClient(
+            os.getenv('MONGOHQ_URL', 'mongodb://localhost:27017/gravity')
+        )
+        self.db = getattr(client, os.getenv('DATABASE_NAME', 'gravity'))
+        self.db.locations.ensure_index([('location', GEOSPHERE)])
+
+        location['coordinates'] = map(float, location['coordinates'])
+
+        try:
+            cached = self.db.locations.find({
+                "location": {
+                    "$nearSphere": location,
+                    '$maxDistance': 500,
+                }}).limit(1)
+            result = cached[0]
+        except:
+            result = None
+        finally:
+            return result
+
+    def query_wolfram(self, location):
+        lat = location['coordinates'][1]
+        lon = location['coordinates'][0]
+
+        client = wolframalpha.Client(self.app_id)
+        self.query = 'gravitational acceleration {} {}'.format(lat, lon)
+        response = client.query(self.query)
+
+        pod = [
+            pod.text for pod in response.pods
+            if pod.text and pod.text.startswith('total field')
+        ][0].split('\n')[0]
+        acceleration = float(pod.split('|')[1].strip().split(' ')[0])
+
+        result = {
+            'acceleration': acceleration,
+            'location': location,
+            'source': self.get_source()
+        }
+
+        try:
+            self.db.locations.insert(result)
+        except:
+            pass
+
+        return result
